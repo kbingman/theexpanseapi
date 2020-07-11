8ea9f1cd1cf5f970a3f082a0fa31d13e026c4c3d
Keith Bingman <keith.bingman@unbounce.com>
2020-07-11 12:44:22 -0700

    extracting functions

--- src/bin/import.rs
+++ src/bin/import.rs
@@ -2,7 +2,7 @@ use std::fs;
 use clap::Clap;
 use serde_json;
 
-use lib::models::{Person, Spacecraft};
+use lib::models::{Person, Spacecraft, SpacecraftClass};
 
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Keith Bingman. <kbingman@gmail.com>")]
@@ -14,8 +14,15 @@ struct Opts {
 enum Output {
   People(Vec<Person>),
   Spacecraft(Vec<Spacecraft>),
+  SpacecraftClass(Vec<SpacecraftClass>),
 }
 
+/// Cleans the filename to provide a key for determining structs
+fn sanitize_key(str: &str) -> String {
+    str.replace("fixtures/", "").replace(".json", "")
+}
+
+/// Converts a JSON string to the given struct type
 fn convert_json<'de, T: serde::de::Deserialize<'de>>(string: &'de str) -> tide::Result<Vec<T>> {
     let data: Vec<T> = serde_json::from_str(string)?;
     Ok(data)
@@ -24,10 +31,12 @@ fn convert_json<'de, T: serde::de::Deserialize<'de>>(string: &'de str) -> tide::
 fn main() -> tide::Result<()> {
     let opts: Opts = Opts::parse();
     let filename = opts.input;
-    let key = filename.replace("fixtures/", "").replace(".json", "");
+    // let re = Regex::new(r"\-\d")?;
+    let key = sanitize_key(&filename);
     let string = fs::read_to_string(&filename)?;
 
     let data: Output = match key.as_str() {
+        "class" => Output::SpacecraftClass(convert_json::<SpacecraftClass>(&string)?),
         "people" => Output::People(convert_json::<Person>(&string)?),
         "spacecraft" => Output::Spacecraft(convert_json::<Spacecraft>(&string)?),
         _ => Output::People(Vec::new()),
